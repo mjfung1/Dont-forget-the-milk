de6df374e663ec3d2ca32422fe6bf5407db36433
mfung <mjfung1@gmail.com>
2020-07-28 17:14:29 -0400

    front end auth

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import SignupContainer from './session/signup_container'
+import { AuthRoute } from '../util/route_utils'
 
 export default () => (
     <div className='greetings'>
         <h1>Dont forget that melkkkkkk!</h1>
-        <Route path="/" component={SignupContainer} />
+        <Route path="/signup" component={SignupContainer} />
     </div>
 );
