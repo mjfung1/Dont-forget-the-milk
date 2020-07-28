794e598e0e40887988bedd731f127da577e61e5b
mfung <mjfung1@gmail.com>
2020-07-28 15:26:50 -0400

    more bugs fixes

--- frontend/actions/session.js
+++ frontend/actions/session.js
@@ -2,7 +2,7 @@ import {
     postUser,
     deleteSession,
     postSession
-} from '../utils/session';
+} from '../util/session';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
